2553d81bb0b41a67de10a4485d026fc90a8397ac
diogoPontes248 <didi248pvc.d.p.v.c@gmail.com>
2026-07-14 19:12:07 -0300

    Add setScoreStrategy method to QuizTemplate class

--- src/Framework/QuizTemplate.java
+++ src/Framework/QuizTemplate.java
@@ -1,15 +1,12 @@
 package Framework;
 
-import Aplicacao.Score;
-
-import java.util.List;
-
 public abstract class QuizTemplate {
     //private List<Question> questionsList;
 
     abstract public void menu();
     abstract public void gameplay();
     abstract public void ending_score();
+    abstract public void setScoreStrategy(ScoreStrategy scoreStrategy);
 
     public void quiz(){
         menu();
