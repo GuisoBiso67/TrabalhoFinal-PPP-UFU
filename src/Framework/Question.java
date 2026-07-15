46ed8d45a3f9fc310cd571d2f8c6d9d014652c7a
diogoPontes248 <didi248pvc.d.p.v.c@gmail.com>
2026-07-14 21:58:38 -0300

    Modify Question constructor to include items list
    Updated constructor to accept a list of items and initialize the items field accordingly.

--- src/Framework/Question.java
+++ src/Framework/Question.java
@@ -7,17 +7,16 @@ public class Question {
     private String question;
     private List<Item> items;
 
-    public Question(String question) {
+    public Question(String question, List<Item> items) {
         this.question = question;
         this.items = new ArrayList<>();
+        for(Item item : items){
+            this.items.add(item);
+        }
     }
 
     public String getStatement() {
         return question;
-    }
-
-    public void addItem(Item i) {
-        this.items.add(i);
     }
 
     public List<Item> getItems() {
